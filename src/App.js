de85dbbcd73d6a057256813d8bb95a4ed617d4a3
Arpit Mishra <arpitmishra.sln123@gmail.com>
2021-11-23 22:09:15 +0530

    second commit added helpers and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import './App.css';
+import React from 'react'
+import Quiz from "./components/Quiz"
+import EndScreen from "./components/EndScreen"
+import Menu from "./components/Menu"
+import {GameStateContext} from "./helpers/Contexts"
+
+
 
 function App() {
+  const [gameState, setGameState] = useState("menu");
+  const [userName, setUserName] = useState("");
+  const [score, setScore ]  = useState(0);
+  
+  
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-     
-          </div>
-  );
+      <h1>Quiz App</h1>
+      <GameStateContext.Provider
+      value = {{
+        gameState,
+        setGameState,
+        userName,
+        score,
+        setScore,
+      }}
+      
+      >
+        {gameState === "menu" && <Menu/>}
+        {gameState === "playing" && <Quiz/>}
+        {gameState === "finished" && <EndScreen/>}
+
+
+      </GameStateContext.Provider>
+
+
+
+
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import "../App.css";
+import { useContext } from "react";
+import {GameStateContext} from "../helpers/Contexts"
+
+
+const Menu = () =>{
+    const {gameState, setGameState, userName, setUserName} = useContext(GameStateContext)
+    
+
+    return(
+
+
+    )
+}
+
+
+export default Menu;
